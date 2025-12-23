8af60f3befda710ee4c063915a825fd99b3c01d4
ksm <tjdalsrnjs10@gmail.com>
2025-12-23 16:04:23 +0900

    init

---
+++ src/main/java/org/example/badhabitzero/BadHabitZeroApplication.java
@@ -0,0 +1,13 @@
+package org.example.badhabitzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadHabitZeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadHabitZeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/badhabitzero/BadHabitZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.badhabitzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadHabitZeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
